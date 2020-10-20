78f42fcdf2a371c0d33d74a96a9ed811e1ed3f7b
Gaurav Singh <acad.grv97@gmail.com>
2020-10-20 19:26:12 +0530

    Frame of app
    Scoreboard, play button, playground, assets, js, css, sounds

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
     echo "let's begin";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Swarm Attack</title>
+    script
+</head>
+<body>
+    
+</body>
+</html>
